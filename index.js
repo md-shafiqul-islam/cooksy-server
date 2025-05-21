36d121cfcb4eebf6087277c1e81e7c73a6f11752
sniekdhoshafiq <developershafiq9578@gmail.com>
2025-05-21 13:07:22 +0600

    connect to MongoDB and secure credentials with environment variables

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
+dotenv.config();
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@sniekdho.hx1c6qw.mongodb.net/?retryWrites=true&w=majority&appName=Sniekdho`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+};
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("CookSy is getting hotter...!!");
