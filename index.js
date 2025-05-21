bc0dae19e89f8c557445f156cfdb5ea19ebea689
sniekdhoshafiq <developershafiq9578@gmail.com>
2025-05-22 00:06:45 +0600

    add POST endpoint to create a new user

--- index.js
+++ index.js
@@ -22,11 +22,19 @@ const client = new MongoClient(uri, {
 const run = async () => {
   try {
     await client.connect();
+
+    const usersCollection = client.db("cookSyDB").collection("users");
+
+    app.post("/users", async (req, res) => {
+      const result = await usersCollection.insertOne(req.body);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log("You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 };
 run().catch(console.dir);
